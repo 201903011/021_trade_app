783670906b4ce710a8485b646aaad5dd5361d991
rahulBioFuel <jsd1@biofuelsjunction.com>
2026-08-03 10:23:39 +0530

    chore: setup changes

--- lib/app/app_bindings.dart
+++ lib/app/app_bindings.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
+import 'package:minimals/app/app_controller.dart';
+import 'package:minimals/screens/funds/controller/funds_controller.dart';
 import 'package:minimals/services/localization_service.dart';
+import 'package:minimals/settings/settings_controller.dart';
 import 'package:minimals/widget/bottom_tabs/botton_tabs_controller.dart';
 
 class AppBindings extends Bindings {
@@ -9,7 +12,8 @@ class AppBindings extends Bindings {
     Get.put<LocalizationService>(LocalizationService(), permanent: true);
     Get.put<BottomTabsController>(BottomTabsController(), permanent: true);
     // Initialize controllers
-    // Get.put<SettingsController>(SettingsController(), permanent: true);
-    // Get.put<AppController>(AppController(), permanent: true);
+    Get.put<SettingsController>(SettingsController(), permanent: true);
+    Get.put<AppController>(AppController(), permanent: true);
+    Get.put<FundsMainController>(FundsMainController(), permanent: true);
   }
 }
